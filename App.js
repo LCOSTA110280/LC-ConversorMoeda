14abd19066eeeecdab9897aa531ee7fc022fb2cd
LCOSTA110280 <LEOCOSRA110280@gMAIL.COM>
2023-02-14 17:01:57 -0300

    Feito !!!!

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Conversor from './src/Conversor';
+
+class App extends Component{
+  render(){
+    return(
+      <View style={styles.container}>
+        <Conversor moedaA="USD" moedaB="BRL" />
+        <Conversor moedaA="EUR" moedaB="BRL" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  container:{
+    flex:1,
     justifyContent: 'center',
-  },
+    alignItems: 'center'
+  }
 });
+
+export default App;
